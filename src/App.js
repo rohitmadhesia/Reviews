e0b77adf63ffa08d9b4b0370ef2f7cbd8eecbdbd
Rohit gupta <rohit2017madhesia@gmail.com>
2022-10-11 00:58:05 +0530

    first commit, initial file added

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Review from './Review';
+import {FaGithub} from 'react-icons/fa'
+
+function App() {
+  return <main>
+    <section className="container">
+      <div className="title">
+        <h2>
+         our review
+        </h2>
+        <div className="underline"></div>
+      </div>
+      <Review/>
+    </section>
+  </main>
+
+  
+
+}
+
+export default App;
